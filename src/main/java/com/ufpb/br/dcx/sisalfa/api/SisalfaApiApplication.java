6ca894fcf73d49c10a6d44a95c1d528ffc3d68ad
matheusnicolas <matheuspb8@gmail.com>
2018-11-12 22:54:01 -0300

    configuração inicial

---
+++ src/main/java/com/ufpb/br/dcx/sisalfa/api/SisalfaApiApplication.java
@@ -0,0 +1,12 @@
+package com.ufpb.br.dcx.sisalfa.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisalfaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisalfaApiApplication.class, args);
+	}
+}
